6f7c8d335d98fcc290cd6fb1325e79a29229d65a
magnuschiu <m6chiu@ucsd.edu>
2024-10-08 06:10:24 +0000

    writing substring

--- main.c
+++ main.c
@@ -48,20 +48,34 @@ int32_t utf8_strlen(char str[]){
 
 int32_t codepoint_index_to_byte_index(char str[], int32_t cpi){
     int codepointindx = 0;
-    for(int byteindx = 0; str[byteindx] != '\0';){ //using byteindex to count instead of i
+    for(int byteindx = 0; str[byteindx] != cpi;){ //using byteindex to count instead of i *edit* i forgot that its better to use cpi cause i know the value
         int charlen = width_from_start_byte(str[byteindx]);
         if (codepointindx == cpi){ //check to see if index has been reached if so then return the index of the byte
             return byteindx;
         }
         byteindx += charlen; //since there are special characters must add them
         codepointindx++; 
+        
+        
     }
     return -1;
 }
 
 void utf8_substring(char str[], int32_t cpi_start, int32_t cpi_end, char result[]){
-
+    if(cpi_start > cpi_end || cpi_end < 0|| cpi_start < 0){
+        return;
+    }
+    int bytestart = codepoint_index_to_byte_index(str, cpi_start);//calling the function to save time
+    int byteend = codepoint_index_to_byte_index(str, cpi_end);
+    int resindx =0;
+    int i = bytestart; //have to declare this i think to ensure str starts at bytestart
+    for(i; i<byteend; i++){ //looping though bytestart to byteend and putting in the characters
+        result[resindx] = str[i];
+        resindx++;
+    }
+    result[resindx] = '\0';
 }
+
 
 
 int main(){
@@ -85,4 +99,13 @@ int main(){
 
     int32_t idx = 4;
     printf("Codepoint index %d is byte index %d\n", idx, codepoint_index_to_byte_index("Joséph", idx));
+
+    char result[17];
+    char str2[] = "🦀🦮🦮🦀🦀🦮🦮";
+    utf8_substring(str2, 3, 7, result);
+    printf("String: %s\nSubstring: %s\n", str2, result); // these emoji are 4 bytes long
+
+    
+    int32_t idx2 = 4;
+    printf("Codepoint at %d in %s is %d\n", idx2, str1, codepoint_at(str1, idx2)); // 'p' is the 4th codepoint  
 }
